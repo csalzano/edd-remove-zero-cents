9cfa5b597ef6ccaccbb3dd1c21d3ed49efc5d8cb
Corey Salzano <938411+csalzano@users.noreply.github.com>
2024-11-11 12:11:43 -0500

    Bug fix. Rewrite to avoid affecting Tax detail lines in checkout. This plugin should only affect the prices of downloads.

--- edd-remove-zero-cents.php
+++ edd-remove-zero-cents.php
@@ -11,8 +11,63 @@ defined( 'ABSPATH' ) or exit;
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
+class Breakfast_EDD_Remove_Zero_Cents {
 
-add_filter( 'edd_format_amount_decimals', function( $decimals, $amount )
-{
-	return ( '.00' == substr( $amount, -3 ) ) ? 0 : $decimals;
-}, 10, 2 );
+	/**
+	 * This variable helps us know when we are outputting taxes in checkout.
+	 *
+	 * @var bool
+	 */
+	protected static $doing_taxes = false;
+
+	/**
+	 * Adds hooks that power the features.
+	 *
+	 * @return void
+	 */
+	public static function add_hooks() {
+		// Only do this once.
+		if ( has_filter( 'edd_format_amount_decimals', array( __CLASS__, 'remove_zero_cents' ) ) ) {
+			return;
+		}
+		add_filter( 'edd_format_amount_decimals', array( __CLASS__, 'remove_zero_cents' ), 10, 2 );
+
+		// Do not affect decimal places in tax detail lines in checkout.
+		add_action( 'edd_checkout_table_tax_first', array( __CLASS__, 'taxes_start' ) );
+		add_action( 'edd_checkout_table_tax_last', array( __CLASS__, 'taxes_stop' ) );
+	}
+
+	/**
+	 * Calculates the number of decimals to use when formatting dollar amounts in
+	 * Easy Digital Downloads.
+	 *
+	 * @param int        $decimals      Default 2. Number of decimals.
+	 * @param int|string $amount        Amount being formatted.
+	 * @return int  The number of decimals places to use when formatting the amount.
+	 */
+	public static function remove_zero_cents( $decimals, $amount ) {
+		if ( self::$doing_taxes ) {
+			return $decimals;
+		}
+		return ( '.00' === substr( $amount, -3 ) ) ? 0 : $decimals;
+	}
+
+	/**
+	 * taxes_start
+	 *
+	 * @return void
+	 */
+	public static function taxes_start() {
+		self::$doing_taxes = true;
+	}
+
+	/**
+	 * taxes_stop
+	 *
+	 * @return void
+	 */
+	public static function taxes_stop() {
+		self::$doing_taxes = false;
+	}
+}
+Breakfast_EDD_Remove_Zero_Cents::add_hooks();
